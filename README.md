43575515db593f5e70b7231e77042345a1637c68
Allen Hart <allenmichaelhart@gmail.com>
2024-01-01 12:01:00 -0500

    Add package code, readme, and license

--- README.md
+++ README.md
@@ -1,49 +1,238 @@
-# Vite TypeScript NPM Package
+# idb-sideloader.js
 
-Scaffold TypeScript npm packages using this template to bootstrap your next library.
+WIP - New Contributors Welcome!
 
-Versions of this template:
-- [Vite TypeScript library npm package](https://github.com/jasonsturges/vite-typescript-npm-package)
-- [Vite JavaScript library npm package](https://github.com/jasonsturges/vite-npm-package)
-- [Rollup TypeScript library npm package](https://github.com/jasonsturges/typescript-npm-package)
-- [Rollup JavaScript library npm package](https://github.com/jasonsturges/npm-package-boilerplate)
+Taking inspiration from [indexed-cache](https://github.com/knadh/indexed-cache), *idb-sideloader* is a tiny Javascript + TypeScript library that "sideloads" static assets (script, link, and img tags) on webpages using the fetch() API and caches them in an IndexedDB store to eliminate the dependency on the standard browser static asset cache, and to eliminate HTTP requests on subsequent page loads. Javascript, CSS, and image assets are stored in IndexedDB as Blobs.
 
+idb-sideloader makes use of [URLSearchParams Polyfill](https://github.com/jerrybendy/url-search-params-polyfill) for backward compatibility with browsers without support for `URLSearchParams.size` (i.e. Safari and some WebKit Chromium builds).
 
-## Getting Started
+### For very specific scenarios only
 
-Begin via any of the following:
+This library is only meant to be used in very specific scenarios.
 
-- Press the "*Use this template*" button
+Unlike the browser's asset cache, IndexedDB is not cleared automatically, providing a longer term static file storage on the client side. The lib uses ES6 (and IndexedDB) and is only expected to work on recent versions of modern browsers. Ideally, this should have been handled with ServiceWorkers, but they don't work in mobile webviews.
 
-- Use [degit](https://github.com/Rich-Harris/degit) to execute:
+Use if at least a few of these are true:
 
-    ```
-    degit github:jasonsturges/vite-typescript-npm-package
-    ```
+- There are large static files (JS, CSS) that rarely change.
+- High traffic from a large number of returning users who access web pages with the same assets regularly and frequently.
+- The pages are mostly inside mobile webviews where browser cache gets evicted  (OS pressure) causing the same assets to be fetched afresh over and over wasting bandwidth.
+- Bandwidth is a concern.
 
-- Use [GitHub CLI](https://cli.github.com/) to execute:
+### Features
 
-    ```
-    gh repo create <name> --template="https://github.com/jasonsturges/vite-typescript-npm-package"
-    ```
+- Supports script, img, link tags.
+- Respects `defer / async` on script tags.
+- Can invalidate cached items with a TTL per tag.
+- Can invalidate cached items with a simple random hash per tag.
 
-- Simply `git clone`, delete the existing .git folder, and then:
+### Gotchas
 
-    ```
-    git clone https://github.com/jasonsturges/vite-typescript-npm-package.git
-    cd vite-typescript-npm-package
-    rm -rf .git
-    git init
-    git add -A
-    git commit -m "Initial commit"
-    ````
-
-There is no package lock included so that you may chose either `npm` or `yarn`.
-
-Remember to use `npm search <term>` to avoid naming conflicts in the NPM Registery for your new package name.
-
+- CORS.
+- First-paint "flash" (needs to be handled manually) as scripts and styles only load after HTML is fetched and rendered by the browser.
+- Browser compatibility.
+- Empty space or line breaks between the opening and closing `<script data-src="remote.js"></script>` tags will be executed as an inline script by the browser, after which the browser will not load the remote script when applied. Ensure that the opening and closing script tags have nothing between then.
+- Scripts that rely on the `document.onload` event will need the event to be triggered for them manually once indexed-cache loads with a `document.dispatchEvent(new Event("load"));`
 
 ## Usage
+
+To cache and sideload static assets:
+
+- Change the original `src` (`href` for CSS) attribute on tags to `data-src`.
+- Give tags a unique ID with `data-key`. The cached items are stored in the database with this key. The actual filenames of the assets can change freely, like in the case of JS build systems.
+- Load and invoke indexed-cache at the end.
+
+#### Example - React + Recoil + SWR + idb-keyval
+
+Use-cases include webapps, PWAs, module federation, and other use-cases. Leverages [idb-keyval](https://github.com/jakearchibald/idb-keyval) for simplicity.
+
+This pattern is useful in situations where a PWA isn't feasible (i.e. extending low-code platforms with federated react component modules).
+
+```tsx
+import { FunctionComponent, useLayoutEffect } from 'react';
+import { atom, selector, useRecoilValue } from 'recoil';
+import { createStore } from 'idb-keyval';
+import IndexedCache from 'src/lib/utils/idb-sideloader';
+
+// see: https://github.com/jakearchibald/idb-keyval/blob/main/custom-stores.md
+export const idbStore = () => createStore('custom-db-name', 'custom-store-name');
+
+export const idbImgStoreAtom = atom({
+  key: 'idb-image-store-atom',
+  default: selector({
+    key: 'idb-image-store-selector',
+    get: async () => {
+      const ic = new IndexedCache({});
+      await ic
+        .init()
+        .then(function () {
+          ic.load();
+        })
+        .catch(function (err) {
+          console.log('error loading indexed-cache', err);
+        });
+      return ic;
+    },
+  }),
+});
+
+export const IndexedCacheInitEffect: FunctionComponent = () => {
+  const cache = useRecoilValue(idbImgStoreAtom);
+  useLayoutEffect(
+    () => {
+      cache
+        .init()
+        .then(() => {
+          cache.load();
+        })
+        .catch(console.error);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [
+      /** once */
+    ]
+  );
+  return null;
+};
+
+export const SelectedImage: FunctionComponent<{
+	imgSrc: string,
+	otherImages: string[]
+}> = ({ imgSrc, otherImages }) => {
+  const cache = useRecoilValue(idbImgStoreAtom);
+	const [selectedImage, setSelectedImage] = useState(imgSrc);
+	useLayoutEffect(() => {
+			cache.load();
+	}, [cache, selectedImage]);
+	return (<>...</>)
+};
+```
+
+
+#### Example - Vanilla HTML + JavaScript
+
+```html
+<!DOCTYPE html>
+<html>
+<head>
+    <title>indexed-cache</title>
+    <meta charset="utf-8" />
+
+    <script data-key="bundle" data-src="bundle_file1234.js"></script>
+
+    <link rel="stylesheet" type="text/css"
+        data-key="style.css"
+        data-src="style.css" />
+</head>
+<body>
+    <h1>indexed-cache</h1>
+
+    <script src="normal-non-side-loaded.js"></script>
+
+    <!--
+        Whenever the value of data-hash changes (eg: from a build system)
+        or the expiry (optional) is crossed, the file is re-fetched
+        and re-cached.
+    //-->
+    <script data-src="sideloaded.js"
+        data-key="mybigsideloadedscript"
+        data-hash="randomhash"
+        data-expiry="2029-03-25T12:00:00-06:30">
+    </script>
+
+    <img data-src="thumb.png"
+        data-key="thumb.png"
+        data-hash="randomhash" />
+
+    <!--
+        Always include and invoke indexed-cache at the end, right before </body>.
+        Use the unpkg CDN or download and host the script locally (dist/indexed-cache.min.js).
+    !-->
+    <script src="https://unpkg.com/@knadh/indexed-cache@0.4.3/dist/indexed-cache.min.js" nomodule></script>
+
+    <!-- Use this if you are supporting old browsers which doesn't support ES6. -->
+    <!-- <script src="https://unpkg.com/@knadh/indexed-cache@0.4.3/dist/indexed-cache.legacy.min.js" nomodule></script> -->
+
+    <script>
+        const ic = new IndexedCache();
+        ic.init().then(function() {
+            ic.load();
+        }).catch(function(err) {
+            console.log("error loading indexed-cache", err)
+        })
+    </script>
+</body>
+</html>
+```
+
+#### Example - Load modern and legacy bundle conditionally
+
+Here is an example on how to load modern(ESM) bundle and legacy bundle conditionally based on browser support.
+
+```html
+    <!-- Only modern browsers understand type=module and legacy browsers will skip this script -->
+    <script type="module">
+        // Use ESM bundle.
+        import IndexedCache from "https://unpkg.com/@knadh/indexed-cache@0.4.3/dist/indexed-cache.esm.min.js";
+        const ic = new IndexedCache();
+        ic.init().then(function() {
+            ic.load();
+        }).catch(function(err) {
+            console.log("error loading indexed-cache", err)
+        })
+    </script>
+
+    <!-- This will only be executed on legacy browsers which doesn't support ES6 modules.
+    Modern browsers ignore the script if its tagged `nomodule`. -->
+    <script src="https://unpkg.com/@knadh/indexed-cache@0.4.4/dist/indexed-cache.legacy.min.js" nomodule></script>
+    <script nomodule>
+        const ic = new IndexedCache();
+        ic.init().then(function() {
+            ic.load();
+
+            // Optionally trigger `onload` if there are scripts that depend on it.
+            // document.dispatchEvent(new Event("load"))
+        }).catch(function(err) {
+            console.log("error loading indexed-cache", err)
+        })
+    </script>
+```
+
+#### Optional configuration
+
+One or more of these optional params can be passed during initialization. Default values are shown below.
+
+```javascript
+new IndexedCache({
+    tags: ["script", "img", "link"],
+    dbName: "indexed-cache",
+    storeName: "objects",
+
+    // If this is enabled, all objects in the cache with keys not
+    // found on elements on the page (data-key) will be deleted.
+    // This can be problematic in scenarios where there are multiple
+    // pages on the same domain that have different assets, some on
+    // certain pages and some on other.
+    prune: false,
+
+    // Enabling this skips IndexedDB caching entirely,
+    // causing resources to be fetched over HTTP every time.
+    // Useful in dev environments.
+    skip: false,
+
+    // Default expiry for an object in minutes (default 3 months).
+    // Set to null for no expiry.
+    expiry: 131400
+}).load();
+```
+
+- `load()` can be called with a DOM Node or NodeList. When none are given, it scans the entire DOM.
+- To manually prune all objects in the database except for a given list of keys, after `await init()`, call `.prune([list of keys])`.
+
+Licensed under the MIT license.
+
+
+## Development
 
 The following tasks are available for `npm run`:
 
@@ -76,7 +265,7 @@ export { distinct } from "./utils/arrayUtils"
 
 
 
-## Development
+## Development Environment
 
 Vite features a host mode to enable development with real time HMR updates directly from the library via the `start` script.
 
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,441 @@
-// Main library exports - these are packaged in your distributable
-export const isOdd = (n: number): boolean => {
-  return !!(n & 1);
+import "url-search-params-polyfill";
+
+function allSettled(promises: Promise<DBObj>[]): Promise<
+  (
+    | {
+        status: "fulfilled";
+        value: DBObj;
+      }
+    | {
+        status: "rejected";
+        reason: Error | string;
+        value?: undefined;
+      }
+  )[]
+> {
+  const wrappedPromises = promises.map((p) =>
+    Promise.resolve(p).then(
+      (val: DBObj) => ({ status: "fulfilled" as const, value: val }),
+      (err: Error | string) => ({
+        status: "rejected" as const,
+        reason: err,
+      }),
+    ),
+  );
+  return Promise.all(wrappedPromises);
+}
+
+interface IndexedCacheOpts {
+  tags: string[];
+  dbName: string;
+  storeName: string;
+  prune: boolean;
+  skip: boolean;
+  expiry: number;
+}
+type CacheableEl = HTMLImageElement | HTMLScriptElement | HTMLLinkElement;
+type DBObj = {
+  el: CacheableEl;
+  key: string;
+  src: string;
+  hash: string;
+  isAsync: boolean;
+  expiry: Date | null;
+  data: {
+    key?: string;
+    blob?: Blob;
+    hash?: string;
+    expiry?: Date | null;
+  };
 };
+export default class IndexedCache {
+  opt: IndexedCacheOpts;
+  db: null | IDBDatabase;
+  constructor(options: Partial<IndexedCacheOpts>) {
+    // if (_icLoaded) {
+    //   throw new Error('indexed-cache is already loaded');
+    // }
+    // _icLoaded = true;
+
+    this.opt = {
+      tags: ["script", "img", "link"],
+      dbName: "indexed-cache",
+      storeName: "objects",
+
+      // If this is enabled, all objects in the cache with keys not
+      // found on elements on the page (data-key) will be deleted by load().
+      // This can be problematic in scenarios where there are multiple
+      // pages on the same domain that have different assets, some on
+      // certain pages and some on other.
+      prune: false,
+
+      // Enabling this skips IndexedDB caching entirely,
+      // causing resources to be fetched over HTTP every time.
+      // Useful in dev environments.
+      skip: false,
+
+      // Default expiry for an object in minutes (default 3 months).
+      // Set to null for no expiry.
+      expiry: 131400,
+
+      ...options,
+    };
+    this.db = null;
+  }
+
+  // This should be called before calling any other methods.
+  async init() {
+    if (this.db) {
+      return;
+    }
+    if (this.opt.skip) {
+      return;
+    }
+    await this._initDB(this.opt.dbName, this.opt.storeName)
+      .then((db) => {
+        this.db = db;
+      })
+      .catch((e) => {
+        console.log("error initializing cache DB. failing over.", e);
+      });
+  }
+
+  // Initialize the DB and then scan and setup DOM elements to cache.
+  async load(elements?: CacheableEl[]) {
+    // This will setup the elements on the page irrespective of whether
+    // the DB is available or not.
+    const objs = await this._setupElements(elements);
+
+    if (!this.db || !objs?.length) {
+      return;
+    }
+
+    // If pruning is enabled, delete all cached elements that are no longer
+    // referenced on the page.
+    if (this.opt.prune) {
+      // Pass the list of keys found on the page.
+      const keys = objs.map((obj) => obj.key);
+      this._prune(keys);
+    }
+  }
+
+  deleteKey(key: string) {
+    if (!this.db) {
+      return;
+    }
+
+    this._store()?.delete(key);
+  }
+
+  // Prune all objects in the DB that are not in the given list of keys.
+  prune(keys: string[]) {
+    this._prune(keys);
+  }
+
+  clear() {
+    if (!this.db) {
+      return;
+    }
+
+    this._store()?.clear();
+  }
+
+  // Initialize the indexedDB database and create the store.
+  _initDB(dbName: string, storeName: string): Promise<IDBDatabase> {
+    return new Promise((resolve, reject) => {
+      if (!window.indexedDB) {
+        reject(new Error("indexedDB is not available"));
+      }
+
+      const req = window.indexedDB.open(dbName);
+
+      // Setup the DB schema for the first time.
+      req.onupgradeneeded = () => {
+        const db = req.result;
+        if (req.transaction && !db.objectStoreNames.contains(storeName)) {
+          db.createObjectStore(storeName, { keyPath: "key" });
+          req.transaction.oncomplete = () => {
+            resolve(db);
+          };
+        }
+      };
+
+      req.onsuccess = () => resolve(req.result);
+
+      req.onerror = () => reject(req.error);
+
+      // Hacky fix for IndexedDB randomly locking up in Safari.
+      setTimeout(() => {
+        if (!this.db) {
+          reject(new Error("Opening IndexedbDB timed out"));
+        }
+      }, 200);
+    });
+  }
+
+  // Scan all matching elements and either:
+  // a) if indexedDB is not available, fallback to loading the assets natively.
+  // b) if DB is available but the object is not cached, fetch(), cache in the DB, and apply the blob.
+  // c) if DB is available and the object is cached, apply the cached blob.
+  // elements should either be null or be a NodeList.
+  async _setupElements(elements?: NodeList | Node[]) {
+    const objs: DBObj[] = [];
+
+    // If there are no elements, scan the entire DOM for groups of each tag type.
+    if (elements instanceof NodeList) {
+      elements = Array.from(elements);
+    } else if (elements instanceof Node) {
+      elements = [elements];
+    } else {
+      const sel = this.opt.tags
+        .map((t) => `${t}[data-src]:not([data-indexed])`)
+        .join(",");
+      elements = document.querySelectorAll(sel);
+    }
+
+    // Get all tags of a particular tag on the page that has the data-src attrib.
+    // document.querySelectorAll(`${tag}[data-src]:not([data-indexed])`).forEach((el) => {
+    Array.prototype.forEach.call(elements, (el) => {
+      if ("indexed" in el.dataset) {
+        return;
+      }
+      const obj: DBObj = {
+        el: el,
+        key: el.dataset.key || el.dataset.src,
+        src: el.dataset.src,
+        hash: el.dataset.hash || el.dataset.src,
+        isAsync:
+          el.tagName !== "SCRIPT" ||
+          el.hasAttribute("async") ||
+          el.hasAttribute("defer"),
+        expiry: null,
+        data: {},
+      };
+
+      // If there is a global expiry or an expiry on the object, compute that.
+      const exp = el.dataset.expiry || this.opt.expiry;
+      if (exp) {
+        obj.expiry = new Date(new Date().getTime() + parseInt(exp) * 60000);
+      }
+
+      objs.push(obj);
+    });
+
+    // If there's no IndexedDB, load all scripts synchronously.
+    if (!this.db) {
+      this._applyElements(objs);
+      return;
+    }
+
+    const promises: Promise<DBObj>[] = [];
+    objs.forEach((obj) => {
+      if (obj.isAsync) {
+        // Load and apply async objects asynchronously.
+        this._getObject(obj)
+          .then((result) => {
+            this._applyElement(obj, result.data.blob);
+          })
+          .catch(() => {
+            this._applyElement(obj);
+          });
+      } else {
+        // Load non-async objects asynchronously (but apply synchronously).
+        promises.push(this._getObject(obj));
+      }
+    });
+
+    if (promises.length === 0) {
+      return objs;
+    }
+
+    // Load all elements successively.
+    await allSettled(promises).then((results) => {
+      // Promise returns [{value: { obj, data }} ...].
+      // Transform to [{ ...obj, data: data} ...]
+      const out = results.reduce((arr, r) => {
+        if (!r.value) return arr;
+        arr.push(r.value);
+        return arr;
+      }, [] as DBObj[]);
+      this._applyElements(out);
+    });
+
+    return objs;
+  }
+
+  // Get the object from the DB and if that fails, fetch() it over HTTP
+  // This function should not reject a promise and in the case of failure,
+  // will return a dummy data object as if it were fetched from the DB.
+  _getObject(obj: DBObj): Promise<DBObj> {
+    return new Promise((resolve) => {
+      // Get the stored blob.
+      this._getDBblob(obj)
+        .then((data) => {
+          resolve({ ...obj, data });
+        })
+        .catch((e) => {
+          // If there is no cause, the object is not cached or has expired.
+          if (e.toString() !== "Error") {
+            console.log("error getting cache blob:", e);
+          }
+
+          // Couldn't get the stored blog. Attempt to fetch() and cache.
+          this._fetchObject(obj)
+            .then((data) => {
+              resolve({ ...obj, data });
+            })
+            .catch(() => {
+              // Everything failed. Failover to loading assets natively.
+              resolve({
+                ...obj,
+                data: {
+                  key: obj.key,
+                  hash: obj.hash,
+                  expiry: obj.expiry,
+                  blob: undefined,
+                },
+              });
+            });
+        });
+    });
+  }
+
+  // Get the blob of an asset stored in the DB. If there is no entry or it has expired
+  // (hash changed or date expired), fetch the asset over HTTP, cache it, and load it.
+  _getDBblob(obj: DBObj): Promise<DBObj["data"]> {
+    return new Promise((resolve, reject) => {
+      try {
+        const req = this._store()?.get(obj.key);
+        if (!req) throw new Error(`DB Key Not Found: ${obj.key}`);
+        req.onsuccess = () => {
+          const data = req.result;
+
+          // Reject if there is no stored data, or if the hash has changed.
+          if (!data || (obj.hash && data.hash !== obj.hash)) {
+            reject(new Error(""));
+            return;
+          }
+
+          // Reject and delete if the object has expired.
+          if (data.expiry && new Date() > new Date(data.expiry)) {
+            this.deleteKey(data.key);
+            reject(new Error(""));
+            return;
+          }
+
+          resolve(data);
+        };
+
+        req.onerror = () => {
+          reject(req.error);
+        };
+      } catch (e) {
+        reject(e);
+      }
+    });
+  }
+
+  // Fetch an asset and cache it.
+  _fetchObject(obj: DBObj): Promise<DBObj["data"]> {
+    return new Promise((resolve, reject) => {
+      fetch(obj.src)
+        .then((r) => {
+          // HTTP request failed.
+          if (!r.ok) {
+            reject(new Error(`error fetching asset: ${r.status}`));
+            return;
+          }
+
+          // Write the fetched blob to the DB.
+          r.blob().then((b) => {
+            const data = {
+              key: obj.key,
+              hash: obj.hash,
+              expiry: obj.expiry,
+              blob: b,
+            };
+
+            // onerror() may not always trigger like in the private mode in Safari.
+            try {
+              const req = this._store()?.put(data);
+              if (!req) throw new Error(`DB Key Not Found: ${obj.key}`);
+              req.onsuccess = () => resolve(data);
+              req.onerror = () => reject(req.error);
+            } catch (e) {
+              reject(e);
+            }
+          });
+        })
+        .catch((e) => reject(e));
+    });
+  }
+
+  // Apply the Blob (if given), or the original obj.src URL to the given element.
+  _applyElement(obj: DBObj, blob?: Blob) {
+    if (!blob) return;
+    let url = obj.src;
+    if (blob) {
+      url = window.URL.createObjectURL(blob);
+    }
+
+    switch (obj.el.tagName) {
+      case "SCRIPT":
+      case "IMG":
+        (obj.el as HTMLImageElement).src = url;
+        break;
+      case "LINK":
+        (obj.el as HTMLLinkElement).href = url;
+    }
+    obj.el.dataset.indexed = "true";
+  }
+
+  // Apply the Blob (if given), or the original obj.src URL to the given list of elements
+  // by chaining each successive element to the previous one's onload so that they load
+  // in order.
+  _applyElements(objs: DBObj[]) {
+    objs.forEach((obj, n) => {
+      if (n >= objs.length - 1) {
+        return;
+      }
+
+      obj.el.onload = obj.el.onerror = () => {
+        this._applyElement(objs[n + 1], objs[n + 1].data.blob);
+      };
+    });
+
+    // Start the chain by loading the first element.
+    this._applyElement(objs[0], objs[0].data.blob);
+  }
+
+  // Delete all objects in cache that are not in the given list of objects.
+  _prune(keys: string[]) {
+    if (!this.db) {
+      return;
+    }
+
+    // Prepare a { key: true } lookup map of all keys found on the page.
+    const keyMap = keys.reduce(
+      (obj, v) => {
+        obj[v] = true;
+        return obj;
+      },
+      {} as Record<string, boolean>,
+    );
+
+    const req = this._store()?.getAllKeys();
+    if (!req) throw new Error(`req object does not exist`);
+    req.onsuccess = () => {
+      req.result.forEach((key) => {
+        if (!((key as string) in keyMap)) {
+          this.deleteKey(key as string);
+        }
+      });
+    };
+  }
+
+  _store() {
+    return this.db
+      ?.transaction(this.opt.storeName, "readwrite")
+      .objectStore(this.opt.storeName);
+  }
+}
